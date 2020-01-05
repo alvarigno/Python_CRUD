1dd55269e982474a04d8614d953a2f2c442cffeb
Álvaro Emparán <alvaro.emparan@gmail.com>
2020-01-05 15:10:28 -0300

    update

--- RestApi/api.py
+++ RestApi/api.py
@@ -1,8 +1,20 @@
-import pyodbc 
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=localhost:1433'
-                      'Database=LibraryDB;'
-                      'Trusted_Connection=yes;'
-                      'UID=sa;PWD=jobver@22;')
+import pyodbc
+server = 'tcp:localhost'
+database = 'LibraryDB'
+username = 'sa'
+password = 'jobver@22'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+cursor.execute("SELECT @@version;")
+row = cursor.fetchone()
+while row:
+    print (row)
+    row = cursor.fetchone()
 
-cursor = conn.cursor()
+#Inser data into SQL Server by Pythonb 
+name = "Book - A"
+price = 125
+    
+insert_records = '''INSERT INTO Books(Name, Price) VALUES(?,?) ''' 
+cursor.execute(insert_records, name, price)
+cnxn.commit()
---
+++ sql-query/Query-sample.sql
@@ -0,0 +1,4 @@
+SELECT TOP (1000) [Id]
+      ,[Name]
+      ,[Price]
+  FROM [LibraryDB].[dbo].[Books]
