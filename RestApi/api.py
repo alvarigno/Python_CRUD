11f3c177aafd54d1a6d792074f7b155ad520e649
Álvaro Emparán <alvaro.emparan@gmail.com>
2020-01-05 15:15:26 -0300

    Update api.py

--- RestApi/api.py
+++ RestApi/api.py
@@ -18,10 +18,12 @@ record_2= ["Book - C", 200]
 record_list = []
 record_list.append(record_1)
 record_list.append(record_2)
-    
+
+""" 
 insert_records = '''INSERT INTO Books(Name, Price) VALUES(?,?) ''' 
 cursor.executemany(insert_records, record_list)
 cnxn.commit()
+"""
 
 # select records
 select_record = '''SELECT * FROM Books'''
@@ -29,3 +31,9 @@ cursor.execute(select_record)
      
 for row in cursor:
     print(row)
+
+
+# update record
+update_query = '''UPDATE Books SET Price = 400 WHERE Id= 2'''
+cursor.execute(update_query)
+cnxn.commit()
